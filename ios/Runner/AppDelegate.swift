be84e551459dcc7767a0bd50dc4a5e4d38e9c4b2
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2021-12-31 20:50:33 +0200

    fix: firestore in ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,12 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    FirebaseApp.configure()
+    if #available(iOS 13.0, *) {
+ 
+    } else {
+        FirebaseApp.configure()
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
